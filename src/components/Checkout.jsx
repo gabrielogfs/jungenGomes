d40831352f8df578113fd44159f88231536735ac
Thiago Ribeiro <tfeliperibeiro@MacBook-Air-de-Thiago.local>
2024-10-10 21:47:45 -0300

    fix: sugestões de ajustes no projeto

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,16 @@
 import React, { useContext } from "react";
 import NavBar from "./NavBar";
 
-export default function Checkout() {
+/*
+Pontos de atenção: 😃
+
+1. Como você está importando arquivos nomeados, você não precisa exportar o componente Checkout como default
+2. A lista de produtos precisa de um key para cada item
+3. Você não está importando o cart do CartContext
+4. Pode remover o React do import, não precisa importar o React se não for usar
+*/
+
+export function Checkout() {
 
     return (
         <>
@@ -11,17 +20,17 @@ export default function Checkout() {
                 {cart.map((product) => (
                     <li>{`${product.name}
                     x${product.quantity} - Valor Total: R$ ${(
-                        product.price * product.quantity
-                    ).toFixed(2).replace('.', ',')} `}</li>
+                            product.price * product.quantity
+                        ).toFixed(2).replace('.', ',')} `}</li>
                 ))}
                 Valor Total:
                 {cart
-                .reduce((prevProductValue, currentProduct) => {
-                    return(
-                        prevProductValue + currentProduct.quantity * currentProduct.price
-                    );
-                },0)
-                .toFixed(2).replace('.', ',')}
+                    .reduce((prevProductValue, currentProduct) => {
+                        return (
+                            prevProductValue + currentProduct.quantity * currentProduct.price
+                        );
+                    }, 0)
+                    .toFixed(2).replace('.', ',')}
             </ul>
         </>
     );
